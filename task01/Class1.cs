be756bae1cd271eef010736177cbb27c5229fd87
kkksarrste <emmptisstery@gmail.com>
2025-06-25 18:28:14 +0300

    Update Class1.cs

--- task01/Class1.cs
+++ task01/Class1.cs
@@ -1,6 +1,22 @@
-﻿namespace task01;
+using System;
+namespace task01;
 
-public class Class1
+public static class StringExtensions
 {
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return false;
 
+        var cleaned = new string(input
+            .ToLower()
+            .Where(c => char.IsLetter(c))
+            .ToArray());
+
+        if (string.IsNullOrEmpty(cleaned))
+            return false;
+
+        var reversed = new string(cleaned.Reverse().ToArray());
+        return cleaned == reversed;
+    }
 }
