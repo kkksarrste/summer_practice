5ad5d1b4f99157421e357a115e7d781164e94af2
kkksarrste <emmptisstery@gmail.com>
2025-06-26 19:52:28 +0300

    Update UnitTest1.cs

--- task02tests/UnitTest1.cs
+++ task02tests/UnitTest1.cs
@@ -1,10 +1,63 @@
-namespace task02tests;
-
-public class UnitTest1
+public class StudentServiceTests
 {
-    [Fact]
-    public void Test1()
-    {
+    private List<Student> _testStudents;
+    private StudentService _service;
 
+    public StudentServiceTests()
+    {
+        _testStudents = new List<Student>
+        {
+            new() { Name = "Иван", Faculty = "ФИТ", Grades = new List<int> { 5, 4, 5 } },
+            new() { Name = "Анна", Faculty = "ФИТ", Grades = new List<int> { 3, 4, 3 } },
+            new() { Name = "Петр", Faculty = "Экономика", Grades = new List<int> { 5, 5, 5 } }
+        };
+        _service = new StudentService(_testStudents);
+    }
+
+    [Fact]
+    public void GetStudentsByFaculty_ReturnsCorrectStudents()
+    {
+        var result = _service.GetStudentsByFaculty("ФИТ").ToList();
+        Assert.Equal(2, result.Count);
+        Assert.True(result.All(s => s.Faculty == "ФИТ"));
+    }
+
+    [Fact]
+    public void GetFacultyWithHighestAverageGrade_ReturnsCorrectFaculty()
+    {
+        var result = _service.GetFacultyWithHighestAverageGrade();
+        Assert.Equal("Экономика", result);
+    }
+
+    [Fact]
+    public void GetStudentsWithMinAverageGrade_FiltersCorrectly()
+    {
+        var result = _service.GetStudentsWithMinAverageGrade(4.5).ToList();
+        Assert.Single(result);
+        Assert.Equal("Иван", result[0].Name);
+    }
+
+    [Fact]
+    public void GetStudentsOrderedByName_ReturnsCorrectOrder()
+    {
+        var result = _service.GetStudentsOrderedByName().ToList();
+        Assert.Equal("Анна", result[0].Name);
+        Assert.Equal("Иван", result[1].Name);
+        Assert.Equal("Петр", result[2].Name);
+    }
+
+    [Fact]
+    public void GroupStudentsByFaculty_CreatesCorrectGroups()
+    {
+        var result = _service.GroupStudentsByFaculty();
+        Assert.Equal(2, result["ФИТ"].Count());
+        Assert.Single(result["Экономика"]);
+    }
+
+    [Fact]
+    public void GetStudentsWithMinAverageGrade_ReturnsEmptyForHighGrade()
+    {
+        var result = _service.GetStudentsWithMinAverageGrade(6.0).ToList();
+        Assert.Empty(result);
     }
 }
