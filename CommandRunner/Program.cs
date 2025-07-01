bdce79d1bdfd1f74e87964c3fe694fa53ee16bbf
kkksarrste <emmptisstery@gmail.com>
2025-07-01 22:29:05 +0300

    Update Program.cs

--- CommandRunner/Program.cs
+++ CommandRunner/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CommandLib;
+using System;
+using System.Reflection;
+
+namespace CommandRunner
+{
+    class Program
+    {
+        static void Main()
+        {
+            var assembly = Assembly.LoadFrom("FileSystemCommands.dll");
+            
+            var directorySizeCommand = CreateCommand(assembly, "DirectorySizeCommand", @"C:\Temp");
+            var findFilesCommand = CreateCommand(assembly, "FindFilesCommand", @"C:\Temp", "*.txt");
+            
+            directorySizeCommand?.Execute();
+            findFilesCommand?.Execute();
+        }
+
+        private static ICommand CreateCommand(Assembly assembly, string commandName, params object[] parameters)
+        {
+            var commandType = assembly.GetType($"FileSystemCommands.{commandName}");
+            return (ICommand)Activator.CreateInstance(commandType, parameters);
+        }
+    }
+}
