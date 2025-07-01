ddc70cd93a913affabe5024762e3aec448a1c59f
kkksarrste <emmptisstery@gmail.com>
2025-07-01 22:56:40 +0300

    Update Class1.cs

--- FileSystemCommands/Class1.cs
+++ FileSystemCommands/Class1.cs
@@ -1,6 +1,45 @@
-﻿namespace FileSystemCommands;
+using CommandLib;
+using System.IO;
 
-public class Class1
+namespace FileSystemCommands
 {
+    public class DirectorySizeCommand : ICommand
+    {
+        public long Size { get; private set; }
+        private readonly string _path;
 
+        public DirectorySizeCommand(string path)
+        {
+            if (!Directory.Exists(path))
+                throw new DirectoryNotFoundException($"Directory {path} not found");
+            _path = path;
+        }
+
+        public void Execute()
+        {
+            Size = 0;
+            foreach (var file in Directory.GetFiles(_path, "*", SearchOption.AllDirectories))
+                Size += new FileInfo(file).Length;
+        }
+    }
+
+    public class FindFilesCommand : ICommand
+    {
+        public string[] FoundFiles { get; private set; }
+        private readonly string _path;
+        private readonly string _mask;
+
+        public FindFilesCommand(string path, string mask)
+        {
+            if (!Directory.Exists(path))
+                throw new DirectoryNotFoundException($"Directory {path} not found");
+            _path = path;
+            _mask = mask;
+        }
+
+        public void Execute()
+        {
+            FoundFiles = Directory.GetFiles(_path, _mask);
+        }
+    }
 }
