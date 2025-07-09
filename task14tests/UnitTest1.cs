25aae24272b7b83f0e6b2d4c4dfdb2cb99fb2423
kkksarrste <emmptisstery@gmail.com>
2025-07-09 21:11:14 +0300

    Update UnitTest1.cs

--- task14tests/UnitTest1.cs
+++ task14tests/UnitTest1.cs
@@ -1,10 +1,8 @@
-namespace task14tests;
+using Xunit;
 
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
+var X = (double x) => x;
+var SIN = (double x) => Math.Sin(x);
 
-    }
-}
+Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2), 1e-4);
+Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8), 1e-4);
+Assert.Equal(10, DefiniteIntegral.Solve(0, 5, X, 1e-6, 8), 1e-5);
