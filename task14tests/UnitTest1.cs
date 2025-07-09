8f41666539e642099e4e97c9ea372b728e61ea8b
kkksarrste <emmptisstery@gmail.com>
2025-07-09 22:49:26 +0300

    Update UnitTest1.cs

--- task14tests/UnitTest1.cs
+++ task14tests/UnitTest1.cs
@@ -1,8 +1,25 @@
 using Xunit;
 
-var X = (double x) => x;
-var SIN = (double x) => Math.Sin(x);
+public class UnitTest1
+{
+    [Fact]
+    public void TestLinearFunction()
+    {
+        var X = (double x) => x;
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2), 1e-4);
+    }
 
-Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2), 1e-4);
-Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8), 1e-4);
-Assert.Equal(10, DefiniteIntegral.Solve(0, 5, X, 1e-6, 8), 1e-5);
+    [Fact]
+    public void TestSinFunction()
+    {
+        var SIN = (double x) => Math.Sin(x);
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8), 1e-4);
+    }
+
+    [Fact]
+    public void TestAnotherLinearCase()
+    {
+        var X = (double x) => x;
+        Assert.Equal(10, DefiniteIntegral.Solve(0, 5, X, 1e-6, 8), 1e-5);
+    }
+}
