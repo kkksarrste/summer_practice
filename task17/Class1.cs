140d7d81237bbee03d0b907ce52668687f144f03
kkksarrste <emmptisstery@gmail.com>
2025-07-18 23:49:56 +0300

    Update Class1.cs

--- task17/Class1.cs
+++ task17/Class1.cs
@@ -1,6 +1,54 @@
-﻿namespace task17;
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
 
-public class Class1
+public interface ICommand
 {
+    void Execute();
+}
 
+public class ServerThread
+{
+    private readonly Thread _thread;
+    private readonly ConcurrentQueue<ICommand> _commandQueue = new();
+    private volatile bool _isRunning;
+
+    public ServerThread(string name)
+    {
+        _thread = new Thread(Run) { Name = name, IsBackground = true };
+    }
+
+    public void Start() => _isRunning = _thread.IsAlive ? false : (_thread.Start(), _isRunning = true).Item2;
+
+    public void AddCommand(ICommand command) => _commandQueue.Enqueue(command);
+
+    private void Run()
+    {
+        while (_isRunning)
+        {
+            if (_commandQueue.TryDequeue(out var cmd))
+                try { cmd.Execute(); } catch { /* Игнорируем ошибки */ }
+            else Thread.Sleep(10);
+        }
+    }
+
+    public void Stop(bool hardStop)
+    {
+        if (hardStop) _isRunning = false;
+        else AddCommand(new SoftStopCommand(this));
+    }
+
+    private class HardStopCommand : ICommand
+    {
+        private readonly ServerThread _server;
+        public HardStopCommand(ServerThread server) => _server = server;
+        public void Execute() => _server._isRunning = false;
+    }
+
+    private class SoftStopCommand : ICommand
+    {
+        private readonly ServerThread _server;
+        public SoftStopCommand(ServerThread server) => _server = server;
+        public void Execute() => _server.Stop(false);
+    }
 }
