d5881da35ccceb0e57001ca73dfc7b45c01e4c14
kkksarrste <emmptisstery@gmail.com>
2025-07-18 23:50:24 +0300

    Update UnitTest1.cs

--- task17tests/UnitTest1.cs
+++ task17tests/UnitTest1.cs
@@ -1,10 +1,41 @@
-namespace task17tests;
+using System;
+using System.Threading;
+using Xunit;
 
-public class UnitTest1
+public class ServerThreadTests
 {
     [Fact]
-    public void Test1()
+    public void HardStop_ImmediatelyTerminates()
     {
+        var server = new ServerThread("Test");
+        bool executed = false;
+        
+        server.AddCommand(new TestCommand(() => executed = true));
+        server.AddCommand(new ServerThread.HardStopCommand(server));
+        server.Start();
+        
+        Thread.Sleep(100);
+        Assert.False(executed);
+    }
 
+    [Fact]
+    public void SoftStop_WaitsForQueue()
+    {
+        var server = new ServerThread("Test");
+        bool executed = false;
+        
+        server.AddCommand(new TestCommand(() => executed = true));
+        server.AddCommand(new ServerThread.SoftStopCommand(server));
+        server.Start();
+        
+        Thread.Sleep(100);
+        Assert.True(executed);
+    }
+
+    private class TestCommand : ICommand
+    {
+        private readonly Action _action;
+        public TestCommand(Action action) => _action = action;
+        public void Execute() => _action();
     }
 }
